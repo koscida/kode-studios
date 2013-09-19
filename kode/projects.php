49697f2699d7a35d237aa011071a8e0d0061d099
britkos@gmail.com <britkos@gmail.com@1ba80d16-4050-948e-81f8-37b7b11098ae>
2013-09-19 00:28:54 +0000

    editing projects section

--- kode/projects.php
+++ kode/projects.php
@@ -2,40 +2,97 @@
 $projects = array(
 	1 => array(
 		"preview_name" => "Friends of MSV",
-		"preview_image" => "images/triangle_1.png",
+		"preview_image" => "",
 		
 		"project_description" => "This was a project for the non-profit organization Friends of Mount Saint Vincent Home. ",
 		"project_link_url" => "http://www.friendsofmsv.org",
 		"project_link_text" => "friendsofmsv.org",
 		"project_skills" => array(
-			"UX and Design",
-			"Database and Backend",
-			"Full implementation",
+			"UX and design",
+			"Responsive layout",
+			"Database support",
+			"Full install",
 		),
 		"project_images" => array(
-			"images/triangle_1.png",
-			"images/triangle_1.png",
+			"",
 		),
 		
 	),
 	2 => array(
 		"preview_name" => "ZOLL Online",
-		"preview_image" => "images/triangle_2.png",
+		"preview_image" => "",
+		
+		"project_description" => "",
+		"project_link_url" => "http://www.zollonline.com",
+		"project_link_text" => "ZOLL Online (current site)",
+		"project_skills" => array(
+			"UX",
+			"Responsive layout",
+		),
+		"project_images" => array(
+			"",
+		),
 	),
 	3 => array(
 		"preview_name" => "Arts & Science",
-		"preview_image" => "images/triangle_3.png",
+		"preview_image" => "",
+		
+		"project_description" => "",
+		"project_link_url" => "",
+		"project_link_text" => "",
+		"project_skills" => array(
+			"UX and Design",
+			"Database and Backend",
+		),
+		"project_images" => array(
+			"",
+		),
 	),
 	4 => array(
 		"preview_name" => "Webscan App",
-		"preview_image" => "images/triangle_4.png",
+		"preview_image" => "",
+		
+		"project_description" => "",
+		"project_link_url" => "",
+		"project_link_text" => "",
+		"project_skills" => array(
+			"UX and design",
+			"Android app",
+		),
+		"project_images" => array(
+			"",
+		),
 	),
 	5 => array(
 		"preview_name" => "Heard Life",
-		"preview_image" => "images/triangle_1.png",
+		"preview_image" => "",
+		
+		"project_description" => "",
+		"project_link_url" => "",
+		"project_link_text" => "",
+		"project_skills" => array(
+			"UX and design",
+			"HTML5 app",
+			"Spec work",
+		),
+		"project_images" => array(
+			"",
+		),
 	),
 	6 => array(
 		"preview_name" => "USCSA Ski Team",
-		"preview_image" => "images/triangle_2.png",
+		"preview_image" => "",
+		
+		"project_description" => "",
+		"project_link_url" => "",
+		"project_link_text" => "",
+		"project_skills" => array(
+			"Complete rebranding",
+			"Site design",
+			"Full implementation",
+		),
+		"project_images" => array(
+			"",
+		),
 	),
 );
